1cc3ef5dd38e0907ec52c95a153455609ad8e026
ShirlyRitika <151626895+ShirlyRitika@users.noreply.github.com>
2025-08-15 12:37:42 +0530

    App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PasswordGenerator from './PasswordGenerator'
+
+const App = () => {
+  return (
+  <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+        <PasswordGenerator/>
+  </div>
+  )
+}
+  export default App
